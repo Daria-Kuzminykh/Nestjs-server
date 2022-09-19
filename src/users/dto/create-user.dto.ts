f586387ec4b6a25e9f868e1cd99a18aba783352b
Daria Kuzminykh <ds.kuzminyh@yandex.ru>
2022-09-19 15:27:33 +0700

    Users and roles

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,9 @@
+import {ApiProperty} from '@nestjs/swagger'
+
+export class CreateUserDto {
+    @ApiProperty({example: 'user@email.com'})
+    readonly email: string;
+
+    @ApiProperty({example: '123456'})
+    readonly password: string;
+}
---
+++ src/users/users.model.ts
@@ -0,0 +1,36 @@
+
+import {Model, Table, Column, DataType, BelongsToMany} from 'sequelize-typescript'
+import {ApiProperty} from '@nestjs/swagger'
+import { Role } from 'src/roles/roles.model';
+import { UserRoles } from 'src/roles/user-roles.model';
+
+interface UserCreationAttrs {
+    email: string,
+    password: string,
+}
+
+@Table({tableName: 'users'})
+export class User extends Model<User, UserCreationAttrs> {
+    @ApiProperty({example: '1'})
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number;
+
+    @ApiProperty({example: 'user@email.com'})
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    email: string;
+
+    @ApiProperty({example: '123456'})
+    @Column({type: DataType.STRING, allowNull: false})
+    password: string;
+
+    @ApiProperty({example: 'false'})
+    @Column({type: DataType.BOOLEAN, defaultValue: false})
+    banned: boolean;
+
+    @ApiProperty({example: 'Why not?'})
+    @Column({type: DataType.STRING, allowNull: true})
+    banReason: string;
+
+    @BelongsToMany(() => Role, () => UserRoles)
+    roles: Role[];
+}

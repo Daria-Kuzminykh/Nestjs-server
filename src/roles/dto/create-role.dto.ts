67a78cf31f4b3b425708f7a3ad73b4d745892f0c
Daria Kuzminykh <ds.kuzminyh@yandex.ru>
2022-09-19 16:32:57 +0700

    Auth module

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,4 +1,9 @@
+import {ApiProperty} from '@nestjs/swagger'
+
 export class CreateRoleDto {
+    @ApiProperty({example: 'ADMIN'})
     readonly value: string;
+
+    @ApiProperty({example: 'Admin'})
     readonly description: string;
 }
--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,16 +1,23 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { RolesService } from './roles.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Role } from './roles.model';
 
+@ApiTags('Роли пользователей')
 @Controller('roles')
 export class RolesController {
     constructor(private rolesService: RolesService) {}
 
+    @ApiOperation({summary: 'Создание роли'})
+    @ApiResponse({status: 200, type: Role})
     @Post()
-    create(@Body() dto: CreateRoleDto) {
-        return this.rolesService.createRole(dto);
+    create(@Body() roleDto: CreateRoleDto) {
+        return this.rolesService.createRole(roleDto);
     }
 
+    @ApiOperation({summary: 'Получение всех ролей'})
+    @ApiResponse({status: 200, type: [Role]})
     @Get('/:value')
     getByValue(@Param('value') value: string) {
         return this.rolesService.getRoleByValue(value);
